b02823f3499f47c7dc1409cc534637e65261c847
Yong4016 <110095387+Yong4016@users.noreply.github.com>
2024-02-12 18:38:20 +1000

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,2 +1,33 @@
 'use strict';
 
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == undefined) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    (filter === 'all') | (filter === project.dataset.type)
+      ? (project.style.display = 'block')
+      : (project.style.display = 'none');
+  });
+
+  projectsContainer.classList.add('animation-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('animation-out');
+  }, 250);
+}
